e3e6d13e87afd38aca782d4a4e86a2e0279c51f8
nikitasasniy <65568663+nikitasasniy@users.noreply.github.com>
2026-02-24 18:52:36 +0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,24 +1,24 @@
-#!/bin/bashbash
-
-
+#!/usr/bin/env bash
 set -e
 
-echo " Installing dependencies"
-echo " "
-pip install numpy pandas scikit-learn --quiet
-echo " "
-echo "Running data creation file"
-echo " "
-python3 lab1/data_creation.py
-echo " "
-echo "Running data preprocessing file"
-echo " "
-python3 lab1/data_preprocessing.py
-echo " "
-echo "Running model preparation file"
-echo " "
-python3 lab1/model_preparation.py
-echo " "
-echo "Running model testing file"
-echo " "
-python3 lab1/model_testing.py
+cd "$(dirname "$0")"
+
+VENV_DIR=".venv"
+
+echo "Starting ML pipeline"
+
+if [ ! -d "$VENV_DIR" ]; then
+  python3 -m venv $VENV_DIR
+fi
+
+source $VENV_DIR/bin/activate
+
+pip install --upgrade pip --quiet
+pip install -r req.txt --quiet
+
+python lab1/data_creation.py
+python lab1/data_preprocessing.py
+python lab1/model_preparation.py
+python lab1/model_testing.py
+
+echo "Pipeline finished successfully"
